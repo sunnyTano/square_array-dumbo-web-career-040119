82d508e9e41f718406878cded52198fae27b35fc
Susan Tan <ssusantan9@gmail.com>
2019-03-24 23:27:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
 def square_array(array)
- 
+ length = array.size
+ counter = 0
+ loop do 
+   if length 
 end
+
+# basket = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
+ 
+# apples_in_basket = basket.size # Step 1
+# apples_taken_out = 0 # Step 2
+ 
+# loop do # Step 3
+#     if apples_taken_out < apples_in_basket 
+#         # Step 4
+#         puts "Taking out #{basket[apples_taken_out]}"
+#         apples_taken_out += 1
+#     else
+#         # Step 5
+#         break
+#     end
+# end
